77804d7b5e7d23addcbfd730b9724b7fa8d0a53f
ajay <ajksharma@gmail.com>
2017-12-02 16:34:53 +0530

    app.java modified

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Ajay Ji!" );
+        System.out.println( "Hello Ajay Sharma!" );
     }
 }
